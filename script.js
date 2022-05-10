7d63c32f87590e3c22ca93467b670774023aa17a
Jonas Kiauleikis <jonas.kiauleikis@gmail.com>
2022-05-10 22:31:28 +0100

    MonkeyTools
    Change from 4 input to 1 input.
    You can copy stright from Excel and it will generate config and email.
    
    CI names will allways come to Uppercase
    
    Added Logo

--- script.js
+++ script.js
@@ -1,35 +1,42 @@
-const genBtn = document.getElementById("button-addon2")
+
 const emailOutput = document.getElementById("email-output")
 const configOutput = document.getElementById("config-output")
 const cBtnEmail = document.getElementById("copy-email-btn")
 const cBtnConf = document.getElementById("copy-conf-btn")
 
-var ciName = document.getElementById("ci-name")
-var deviceType = document.getElementById("device-name")
-var custNo = document.getElementById("customer-name")
-var pmName = document.getElementById("pm-name")
+//for main input field
+const mainInput = document.getElementById("mainInput")
+const applyBtn = document.getElementById("mainInputBtn")
 
 
-// Main button to generate config and email
-genBtn.onclick = function(){
-    console.log(ciName.value)
-    console.log(deviceType.value)
-    console.log(custNo.value)
-    console.log(pmName.value)
-    return emailOutput.innerHTML = genEmail(), configOutput.innerHTML = genConf()
-    
-    // var eText = emailOutput.innerText("shudas")
-    // return eText
+//for secondary input field
+// var ciName = document.getElementById("ci-name")
+// var deviceType = document.getElementById("device-name")
+// var custNo = document.getElementById("customer-name")
+// var pmName = document.getElementById("pm-name")
+
+
+applyBtn.onclick = function(){
+
+    mainValue = mainInput.value
+    const mainArray = mainValue.split("\t")
+    ciName = mainArray[0].toUpperCase()
+    deviceType = mainArray[3]
+    pickRef = mainArray[5]
+    custNo = mainArray[6]
+    pmName = mainArray[9]   
+    return emailOutput.innerHTML = genEmail(), configOutput.innerHTML = genConf();
 }
+
 // function for email
 function genEmail(){
-    gemail = `Hi ${pmName.value},\n\nFollowing has been configured, boxed, and placed on the shipping shelf:\n\n${ciName.value}\n${deviceType.value}\n\nKind regards,\nJonas`
-    return gemail
+    gemail = `Hi ${pmName},\n\nThe following has been configured, boxed, and placed on the shipping shelf:\n\n${ciName}\n${deviceType}\n${pickRef}\n\nKind regards,\nJonas`
+    return gemail;
 }
 //function for config
 function genConf(){
-    config = `Prashau:\n${ciName.value}\n${deviceType.value}\n${custNo.value}\n${pmName.value}`
-    return config
+    config = `Prashau:\n${ciName}\n${deviceType}\n${custNo}\n${pmName}`;
+    return config;
 }
 
 
@@ -44,9 +51,4 @@ cBtnConf.onclick = function(){
 }
 
 
-
-
-function genConfig(){
-
-}
 
