48736f3fc47994dc31f8bd800ef880178b701afb
Jonas Kiauleikis <jonas.kiauleikis@gmail.com>
2022-06-13 10:57:21 +0100

    Updated Class List, changed FTP-IP variable

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const cBtnConf = document.getElementById("copy-conf-btn")
 //for main input field box
 const mainInput = document.getElementById("mainInput")
 const applyBtn = document.getElementById("mainInputBtn")
+
+//FTP server IP
+const ftpServerIp = "192.168.1.1"
 //Function to create classes for device:OS
 class device {
     constructor(deviceName, operatingSystem,firmware, bootString){
@@ -15,30 +18,21 @@ class device {
     }
 }
 //new classes for device and os
-const c927_4p = new device(
-    "Cisco C927-4P","c900-universalk9-mz.SPA.159-3.M3.bin", "c900_VA_A_39x3_B_39x3_26u.bin")
-const isr4331 = new device(
-    "ISR4331/K9", "isr4300-universalk9.16.09.06.SPA.bin", "")
-const ws_c2960_24pc_s = new device(
-    "WS-C2960-24PC-S","c2960-lanlitek9-mz.122-55.SE12.bin")
-const ws_c2960l_48ps_ll = new device(
-    "WS-C2960L-48PS-LL", "c2960l-universalk9-mz.152-7.E0a.bin")
-const ws_c2960x_48lps_l = new device(
-    "WS-C2960X-48LPS-L","c2960x-universalk9-mz.152-7.E4.bin")
-const c891_k9 = new device(
-    "Cisco C891-K9", "c890-universalk9-mz.158-3.M6.bin")
-const c1111_4p = new device(
-    "Cisco C1111-4P", "c1100-universalk9_ias.16.09.06.SPA.bin"
-)
-
 let totalOfDevices = [
-    c927_4p,
-    isr4331,
-    ws_c2960_24pc_s,
-    ws_c2960l_48ps_ll,
-    ws_c2960x_48lps_l,
-    c891_k9,
-    c1111_4p
+    c927_4p = new device(
+    "Cisco C927-4P","c900-universalk9-mz.SPA.159-3.M3.bin", "c900_VA_A_39x3_B_39x3_26u.bin"),
+    isr4331 = new device(
+    "ISR4331/K9", "isr4300-universalk9.16.09.06.SPA.bin", ""),
+    ws_c2960_24pc_s = new device(
+    "WS-C2960-24PC-S","c2960-lanlitek9-mz.122-55.SE12.bin"),
+    ws_c2960l_48ps_ll = new device(
+    "WS-C2960L-48PS-LL", "c2960l-universalk9-mz.152-7.E0a.bin"),
+    ws_c2960x_48lps_l = new device(
+    "WS-C2960X-48LPS-L","c2960x-universalk9-mz.152-7.E4.bin"),
+    c891_k9 = new device(
+    "Cisco C891-K9", "c890-universalk9-mz.158-3.M6.bin"),
+    c1111_4p = new device(
+    "Cisco C1111-4P", "c1100-universalk9_ias.16.09.06.SPA.bin")
 ]
 console.log(totalOfDevices)
 //BUTTON CLICK function
@@ -74,9 +68,9 @@ applyBtn.onclick = function(){
         bootString = `conf t\nboot system flash bootflash:${osName}\nend\nwr mem\nreload\n\n`
     }
     //Variables for different part of configs
-    osDownload = `copy tftp://172.17.2.1/${osName} flash:\n\n`
+    osDownload = `copy tftp://${ftpServerIp}/${osName} flash:\n\n`
     configDownload = `copy tftp://customer-conf/${custNo}\n\n`
-    licenseInstall = "license install tftp://172.17.2.1/licenses/.lic"
+    licenseInstall = `license install tftp://${ftpServerIp}/licenses/.lic`
     //ALL CONFIG generated to be outputed
     allConfigOutput = primaryConfig+osDownload+bootString+configDownload+licenseInstall
     //Main return for outputs   
